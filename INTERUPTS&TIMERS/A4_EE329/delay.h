bbda5d685a601105ecd0f99f8aa2dfa9647e7bd6
Jordan Reichhardt <jordan.reichhardt@gmail.com>
2026-06-19 18:46:25 -0700

    Portfolio import: add INTERUPTS&TIMERS/A4_EE329/delay.h
    Portfolio reorganization note: this commit imports one previously completed source file into a public class/project repository. The commit date recorded by Git is the actual portfolio import date.

---
+++ INTERUPTS&TIMERS/A4_EE329/delay.h
@@ -0,0 +1,7 @@
+#ifndef INC_DELAY_H_
+#define INC_DELAY_H_
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_it.h"
+void SysTick_Init(void);
+void delay_us(const uint32_t time_us);
+#endif /* INC_DELAY_H_ */
