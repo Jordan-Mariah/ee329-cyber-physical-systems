c9e4ad3c80a4d520ad05517b888183791947dcd6
Jordan Reichhardt <jordan.reichhardt@gmail.com>
2026-06-19 18:46:27 -0700

    Portfolio import: add LCD/main.h
    Portfolio reorganization note: this commit imports one previously completed source file into a public class/project repository. The commit date recorded by Git is the actual portfolio import date.

---
+++ LCD/main.h
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * EE 329 A3 LCD TIMER
+ *******************************************************************************
+ * @file           : main.h
+ * @brief          : Keypad and LCD interfaced to create a countdown timer
+ * project         : EE 329 F'24 Assignment 3
+ * authors         : Mateo Charles - vcharles@calpoly.edu
+ * date            : 10/14/24
+ * compiler        : STM32CubeIDE v.1.12.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines and function
+ *                   declarations of the application.
+ *****************************************************************************/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+void reset_timer ( void );
+static void MX_GPIO_Init( void );
+
+/* Private defines -----------------------------------------------------------*/
+#define B1_Pin GPIO_PIN_13
+#define B1_GPIO_Port GPIOC
+#define LD3_Pin GPIO_PIN_14
+#define LD3_GPIO_Port GPIOB
+#define USB_OverCurrent_Pin GPIO_PIN_5
+#define USB_OverCurrent_GPIO_Port GPIOG
+#define USB_PowerSwitchOn_Pin GPIO_PIN_6
+#define USB_PowerSwitchOn_GPIO_Port GPIOG
+#define STLK_RX_Pin GPIO_PIN_7
+#define STLK_RX_GPIO_Port GPIOG
+#define STLK_TX_Pin GPIO_PIN_8
+#define STLK_TX_GPIO_Port GPIOG
+#define USB_SOF_Pin GPIO_PIN_8
+#define USB_SOF_GPIO_Port GPIOA
+#define USB_VBUS_Pin GPIO_PIN_9
+#define USB_VBUS_GPIO_Port GPIOA
+#define USB_ID_Pin GPIO_PIN_10
+#define USB_ID_GPIO_Port GPIOA
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define LD2_Pin GPIO_PIN_7
+#define LD2_GPIO_Port GPIOB
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
